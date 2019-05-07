39f1998141e75b1a036724ad52d7481f1b47583f
ziyi.hzy <ziyi.hzy@alibaba-inc.com>
2019-05-07 17:32:51 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export const getPlugin = () => import("./plugin");
+
+export const getConfig = () => ({
+  name: "pri-plugin-pri-plugin-request"
+});
---
+++ src/plugin/index.ts
@@ -0,0 +1,34 @@
+import * as path from 'path';
+import { pri, tempTypesPath } from 'pri';
+import * as fs from 'fs-extra';
+
+const requestFilePath = path.join(pri.projectRootPath, tempTypesPath.dir, 'request.ts');
+
+/** Support pri/models alias */
+pri.build.pipeConfig(config => {
+  if (!config.resolve.alias) {
+    config.resolve.alias = {};
+  }
+
+  config.resolve.alias['pri/request'] = requestFilePath;
+
+  return config;
+});
+
+pri.project.onCreateEntry(async () => {
+  const requestContent = `
+      import request from 'pri-request'
+      export { request }
+    `;
+
+  const prettier = await import('prettier');
+
+  // If has stores, create helper.ts
+  fs.outputFileSync(
+    requestFilePath,
+    prettier.format(requestContent, {
+      semi: false,
+      parser: 'typescript'
+    })
+  );
+});
